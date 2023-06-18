3fa6ca44750710488b91d6d097164e6b259cd938
Niloydey59 <niloydev59@gmail.com>
2023-06-18 10:51:48 +0600

    Day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products=[
+  Item(
+      "Codepur",
+      "Iphone 12 pro",
+      "Apple Iphone 12th generation",
+      999,
+      "#3350a",
+      "https://adminapi.applegadgetsbd.com/storage/media/large/1527-30875.jpg"
+  )
+];
